854dcf2e08008a335b697bd83fd6ccd03c5fee55
Sonata Arrington <sarrington1009@gmail.com>
2020-01-22 23:35:58 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,6 +17,18 @@ def directors_totals(nds)
   # ...
   #
   #
-  # Be sure to return the result at the end!
-  nil
+  results = {}
+  counter = 0
+  while counter < nds.count do
+    director_name = nds[counter][:name]
+    result[director_name] = 0  
+    movie_info = 0
+    while movie_info < nds[counter][:movies].count do
+      
+     result[director_name] += nds[counter][:movies][movie_info][:worldwide_gross]
+      movie_info += 1
+    end 
+    counter += 1
+  end
+  result
 end
